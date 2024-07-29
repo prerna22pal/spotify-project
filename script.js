a9be2fa8efd0596384a74f920e8e55db7fa5bb25
prerna22pal <palprerna152@gmail.com>
2024-07-29 17:57:43 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("please keep it... #SKRM")
+
+async function main(){
+  let a =await fetch("https://pagalworld.gay/punjabi-latest-updates/1.html");
+  let response = await a.text();
+  console.log(response);
+}
+main()
